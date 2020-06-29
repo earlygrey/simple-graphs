fff39b4b78666bf2f0bd2c49f45dfc44c426d8b3
earlygrey <contact@earlygrey.space>
2020-06-29 10:53:07 +0200

    add javadoc

--- src/main/java/space/earlygrey/simplegraphs/DirectedGraph.java
+++ src/main/java/space/earlygrey/simplegraphs/DirectedGraph.java
@@ -26,10 +26,23 @@ public class DirectedGraph<V> extends Graph<V> {
         return new DirectedGraph<>();
     }
 
+    /**
+     * Sort the vertices of this graph in topological order. That is, for every edge from vertex u to vertex v, u comes before v in the ordering.
+     * This is reflected in the iteration order of the collection returned by {@link #getVertices()}.
+     * Note that the graph cannot contain any cycles for a topological order to exist. If a cycle exists, this method will do nothing.
+     * @return true if the sort was successful, false if the graph contains a cycle
+     */
     public boolean topologicalSort() {
         return algorithms.topologicalSort();
     }
 
+    /**
+     * Perform a topological sort on the graph, and puts the sorted vertices in the supplied list.
+     * That is, for every edge from vertex u to vertex v, u will come before v in the supplied list.
+     * Note that the graph cannot contain any cycles for a topological order to exist. If a cycle exists, the sorting procedure will
+     * terminate and the supplied list will only contain the vertices up until the point of termination.
+     * @return true if the sort was successful, false if the graph contains a cycle
+     */
     public boolean topologicalSort(List<V> sortedVertices) {
         return algorithms.topologicalSort(sortedVertices);
     }
